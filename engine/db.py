bd7c78041d33e2d130c5e5ce70d7a5359733b575
prathammhavale <prathammhavale38@gmail.com>
2024-10-24 17:08:21 +0530

    ai_assistant

---
+++ engine/db.py
@@ -0,0 +1,48 @@
+import csv
+import sqlite3
+
+con = sqlite3.connect("jarvis.db")
+cursor = con.cursor()
+
+query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
+cursor.execute(query)
+
+query = "INSERT INTO sys_command VALUES (null,'pictures', 'C:\\Users\\lenovo\\Desktop\\drawings\\shisui-uchiha.jpg')"
+cursor.execute(query)
+con.commit()
+
+
+# query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'wikipedia', 'https://www.wikipedia.org/')"
+# cursor.execute(query)
+# con.commit()
+
+
+# testing module
+# app_name = "android studio"
+# cursor.execute('SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+# results = cursor.fetchall()
+# print(results[0][0])
+
+# Create a table with the desired columns
+#cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+
+
+# Specify the column indices you want to import (0-based index)
+# Example: Importing the 1st and 3rd columns
+# desired_columns_indices = [0, 30]
+
+
+# # Commit changes and close connection
+# con.commit()
+# con.close()
+
+# query = "INSERT INTO contacts VALUES (null,'pawan', '1234567890', 'null')"
+# cursor.execute(query)
+# con.commit()
+
+# query = 'kunal'
+# query = query.strip().lower()
+
---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import eel 
+
+from engine.features import *
+from engine.command import *
+
+
+def start():
+    
+    eel.init("www")
+
+    playAssistantSound()
+    @eel.expose
+    def init():
+        subprocess.call([r'device.bat'])
+        eel.hideLoader()
+        speak("Hello, Welcome Sir, How can I help you")
+        eel.hideStart()
+        playAssistantSound()
+
+    os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+    eel.start('index.html', mode=None, host='localhost', block=True)
+
